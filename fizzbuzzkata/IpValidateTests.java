6cf2b0258c1911ff06fdbdeb5cde67f63d84c38a
SanderTLN <62286818+SanderTLN@users.noreply.github.com>
2021-11-18 20:06:05 +0200

    Add failing test 3

--- fizzbuzzkata/IpValidateTests.java
+++ fizzbuzzkata/IpValidateTests.java
@@ -1,5 +1,6 @@
 package fizzbuzzkata;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,5 +18,12 @@ class IpValidateTests {
 	void ShouldBe_False_GivenStringWithThreeDots() {
 		IpValidate validator = new IpValidate();
 		assertTrue(validator.ValidateIpv4Address("1.2.3.1"));
+	}
+	
+	@Test
+	void ShouldReturnFourNumbersGivenStringWithThreeDots() {
+		IpValidate validator = new IpValidate();
+		int[] expected = {1,2,3,4};
+		assertArrayEquals(expected,validator.getNumbers("1.2.3.4"));
 	}
 }
