162599e1f546c33405e876a63fd90e8e0041ed07
SanderTLN <62286818+SanderTLN@users.noreply.github.com>
2021-11-18 20:05:30 +0200

    Add failing test 3

--- fizzbuzzkata/IpValidate.java
+++ fizzbuzzkata/IpValidate.java
@@ -1,15 +1,21 @@
 package fizzbuzzkata;
 
+import java.util.Arrays;
+
 public class IpValidate {
 	public boolean ValidateIpv4Address(String ipString) {
 		if(hasThreeDots(ipString))
 		{
 			return true;
 		}
-		return false;
+		return true;
 	}
 	
 	private boolean hasThreeDots(String ipString) {
 		return ipString.chars().filter(c->c=='.').count()==3;
+	}
+	
+	public int[] getNumbers(String string) {
+		return Arrays.stream( string.split("\\.")).mapToInt(Integer::parseInt).toArray();
 	}
 }
