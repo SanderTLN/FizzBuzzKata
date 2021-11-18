17693e7081f5f0a4d40fc9af6fe5634869067e0e
SanderTLN <62286818+SanderTLN@users.noreply.github.com>
2021-11-18 20:00:41 +0200

    Add failing tests

--- fizzbuzzkata/IpValidateTests.java
+++ fizzbuzzkata/IpValidateTests.java
@@ -1,9 +1,14 @@
 package fizzbuzzkata;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
 class IpValidateTests {
-	
+	@Test
+	void ShouldReturnFalse_GivenEmptyStringParameter() {
+		IpValidate validator = new IpValidate();
+		assertFalse(validator.ValidateIpv4Address(""));
+	}
 }
