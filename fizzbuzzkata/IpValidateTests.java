1428f36a3a45e1d964cf40fd61aeb8ee0e0f39b1
SanderTLN <62286818+SanderTLN@users.noreply.github.com>
2021-11-18 20:15:22 +0200

    Add failing test 7

--- fizzbuzzkata/IpValidateTests.java
+++ fizzbuzzkata/IpValidateTests.java
@@ -44,7 +44,13 @@ class IpValidateTests {
 	
 	@Test
 	void ShouldBe_False_GivenIPstartingWithZero() {
-		IpValidate validator= new IpValidate();
+		IpValidate validator = new IpValidate();
 		assertFalse(validator.ValidateIpv4Address("0.2.3.4"));
+	}
+	
+	@Test
+	void ShouldBe_False_GivenIPendingWithZero() {
+		IpValidate validator = new IpValidate();
+		assertTrue(validator.ValidateIpv4Address("1.2.3.0"));
 	}
 }
